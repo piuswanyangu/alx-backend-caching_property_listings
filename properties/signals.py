b228462fe4282068fd6e3922809f9ef7f2b321d7
pius wanyangu <ndubipius96@gmail.com>
2026-01-09 18:32:50 +0300

    Invalidate property cache using Django signals

---
+++ properties/signals.py
@@ -0,0 +1,14 @@
+from django.db.models.signals import post_delete, post_save
+from django.dispatch import receiver
+from django.core.cache import cache
+
+from .models import Property
+
+@receiver(post_save, sender=Property)
+def invalidate_property_cache_on_save(sender,instance, **kwargs):
+    cache.delete("all_properties")
+
+
+@receiver(post_delete, sender=Property)
+def invalidate_property_cache_on_delete(sender,instance, **kwargs):
+    cache.delete("all_properties")
