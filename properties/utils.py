fccda3b6d9744741dd99e2f063d6eae7ef1c22c0
pius wanyangu <ndubipius96@gmail.com>
2026-01-09 18:59:19 +0300

    Add Redis cache hit/miss metrics and analysis

--- properties/utils.py
+++ properties/utils.py
@@ -1,5 +1,8 @@
 from django.core.cache import cache
 from .models import Property
+import logging
+from django.redis import get_redis_connection
+logger = logging.getLogger(__name__)
 
 def get_all_properties():
     cached_properties = cache.get("all_properties")
@@ -9,3 +12,28 @@ def get_all_properties():
         cache.set("all_properties", cached_properties)
 
     return cached_properties
+
+def get_redis_cache_metrics():
+    redis_conn = get_redis_connection("default")
+
+    info = redis_conn.info()
+
+    hits = info.get("keyspace_hits", 0)
+    misses = info.get("keyspace_misses",0)
+
+    total = hits + misses
+    hit_ratio = hits / total if total > 0 else 0
+
+    metrics = {
+        "keyspace_hits": hits,
+        "keyspace_misses": misses,
+        "hit_ratio": hit_ratio,
+    }
+    logger.info(
+        "Redis cache Metrics | Hits: %s | Misses: %s | Hit Ratio: %.2f",
+        hits,
+        misses,
+        hit_ratio,
+    )
+    return  metrics
+
